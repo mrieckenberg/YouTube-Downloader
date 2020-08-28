f4878b5e42d580ab5950c52ef788a4ebac0607e8
18mpr1 <matthew.paul.rieckenberg@gmail.com>
2020-08-27 22:29:24 -0400

    An evening's worth of work.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from pytube import YouTube
 from tkinter import *
+import webbrowser
+from tkinter import filedialog
+import os
 
 # Root
 root = Tk()
@@ -18,26 +21,47 @@ root.title("YouTube Downloader")
 entry1 = Entry(root,bg="#DAF7A6",fg="#3D0909",width='20')
 
 # Definitions
-def downloadVideo():
-    pass
+
+
+def onClick(x):
+    webbrowser.open(x,new=1)
 
 
 def Submit():
     iWindow = Toplevel(root)
-    iWindow.geometry('1000x100')
+    iWindow.geometry('1000x200')
     iWindow.config(bg='black')
-    url = 'https://www.youtube.com/watch?v=gQrkvZeE3Uc'
+
+    def search_for_file_path():
+        currdir = os.getcwd()
+        tempdir = filedialog.askdirectory(parent=root, initialdir=currdir, title='Please select a directory')
+        if len(tempdir) > 0:
+            print("You chose: %s" % tempdir)
+        return tempdir
+    search_for_file_path()
+    file_path_variable = search_for_file_path()
+    print("\nfile_path_variable = ", file_path_variable)
+
+    def downloadVideo():
+        url = 'https://www.youtube.com/watch?v=YaG5SAw1n0c'
+        yt = YouTube(url)
+        stream = yt.streams.first()
+        stream.download()
+        stream.download(file_path_variable)
+
+    url = 'https://www.youtube.com/watch?v=YaG5SAw1n0c'
     yt = YouTube(url)
     vidTitle = Label(iWindow,bg='black',fg='yellow',text=yt.title,font=("Georgia",17,'bold'))
     vidTitle.pack()
     vidURL = Label(iWindow,bg='black',fg='yellow',text=url,font=("Georgia",17,'bold'))
     vidURL.pack()
-    downloadButton = Button(iWindow,text="Download",font=("Georgia",10,'bold'),bg='#09DBD3',fg='#B8024C')
+    downloadButton = Button(iWindow,text="Download",font=("Georgia",10,'bold'),bg='#09DBD3',fg='#B8024C',command=downloadVideo)
     downloadButton.pack()
+    click = Button(iWindow,text="See video",bg="#E53F83",fg='white',font=("Georgia",12,'bold'),command=lambda: onClick(url))
+    click.pack()
 
 
-
-
+Submit()
 
 
 
